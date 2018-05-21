10e73a3f918db4927eaa97baa06094610dbf5e5c
Ricardo Nunes <ricardo.nunes.isla@gmail.com>
2018-05-21 22:26:59 +0100

    Changed the code in Diff2Arrays with a new approach. Created a new single array with the size of the sum in the 2 arrays length and sorted it out the single array.
    Next step is to check for the different word.

--- Algorithms/Algorithms/Intermediate/Diff2Arrays.cs
+++ Algorithms/Algorithms/Intermediate/Diff2Arrays.cs
@@ -11,6 +11,29 @@ namespace Algorithms.Intermediate
         public static T[] Diff(T[] arr1, T[]arr2)
         {
             List<T> list = new List<T>();
+            T[] singleArr = new T[arr1.Length + arr2.Length];
+
+            for (int i = 0; i != arr1.Length; i++)
+                singleArr[i] = arr1[i];
+
+            int x = 0;
+            int sizeArr1 = arr1.Length;
+            while (x != arr2.Length)
+            {
+                if (singleArr[sizeArr1] == null)
+                    singleArr[sizeArr1] = arr2[x];
+
+                sizeArr1++;
+                x++;
+            }
+
+            Array.Sort(singleArr);
+
+            //continue with searching the array for the differente word(s)
+         
+
+
+
 
             //int max = Math.Max(arr1.Length, arr2.Length);
 
@@ -20,30 +43,9 @@ namespace Algorithms.Intermediate
             //        if (!arr1[i].Equals(arr2[i]))
             //            list.Add(arr1[i]);
             //}
-            Array.Sort(arr1);
-            Array.Sort(arr2);
+            //Array.Sort(arr1);
+            //Array.Sort(arr2);
 
-            if (arr1.Length >= arr2.Length)
-            {
-                for (int i = 0; i != arr1.Length; i++)
-                    for (int j = 0; j != arr2.Length; j++)
-                        if (i <= arr2.Length - 1)
-                        {
-                            if (arr1[i].Equals(arr2[j]))
-                                continue;
-                            if (!arr1[i].Equals(arr2[j]))
-                                list.Add(arr1[i]);
-                        }
-                            
-            }
-            else
-            {
-                for (int i = 0; i != arr2.Length; i++)
-                    for (int j = 0; j != arr1.Length; j++)
-                        if (i <= arr1.Length - 1)
-                            if (!arr2[i].Equals(arr1[j]))
-                                list.Add(arr2[i]);
-            }
                                 
 
 
